299960257d6e0fc7689501ba1a6c75a97cead30d
Rohith Pudari <27728974+rohithpudari@users.noreply.github.com>
2026-08-06 23:49:16 -0400

    fix integration test

--- test/integration_comments.sh
+++ test/integration_comments.sh
@@ -21,7 +21,7 @@ YAML
 bundle exec jekyll build --config "_config.yml,${tmp_override}" -d "${tmp_site}" >/dev/null
 
 # giscus_page="${tmp_site}/blog/2022/giscus-comments/index.html"
-giscus_page="${tmp_site}/blog/2023/copilot-to-pilot/index.html"
+giscus_page="${tmp_site}/blog/2023/masters/index.html"
 disqus_page="${tmp_site}/blog/2015/disqus-comments/index.html"
 
 grep -q 'https://giscus.app/client.js' "${giscus_page}"
